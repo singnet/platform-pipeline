e0a4d3723bc5350f8ace6097ce80d92d03ef2d96
Alexander Scherbatiy <stellarspot@gmail.com>
2018-09-17 16:16:27 +0300

    Add SingularityNET job is created integration test

--- features/publish_example_service.feature
+++ features/publish_example_service.feature
@@ -7,15 +7,18 @@ Scenario: Run all services and publish example service
 	Given Identity is created with user "snet-user" and private key "0xc71478a6d0fe44e763649de0a0deb5a080b788eefbbcf9c6f7aef0dd5dbd67e0" 
 	Given snet is configured with Ethereum RPC endpoint 8545 
 	Given snet is configured with IPFS endpoint 5002
-	Given Organization is added:
+	When Organization is added:
 		| organization | address | member |
 		| ExampleOrganization | 0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2 | 0x3b2b3c2e2e7c93db335e69d827f3cc4bc2a2a2cb |
-	Given  example-service is registered
+	When  example-service is registered
 		| name | price | endpoint | tags | description |
 		| ExampleOrganization | 1 | http://localhost:8080 | example service | Example service | 
-	Given  example-service is published to network
+	When  example-service is published to network
 		| agent factory address | registry address |
 		| 0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e | 0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2 | 
-	Given example-service is run with snet-daemon
+	When example-service is run with snet-daemon
 		| daemon port | ethereum endpoint port| passthrough endpoint port | agent contract address | private key |
 		| 8080 | 8545 | 5001 | 0xD39321C654351b412F4D13B45E7020FE9f99f608 | ba398df3130586b0d5e6ef3f757bf7fe8a1299d4b7268fdaae415952ed30ba87 | 
+	Then SingularityNET job is created
+		| max price | agent contract address | 
+		| 100000000 | 0xD39321C654351b412F4D13B45E7020FE9f99f608 | 
--- publish_example_service_test.go
+++ publish_example_service_test.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var output_file string
@@ -339,12 +340,58 @@ func exampleservice_is_run_with_snet_daemon(table *gherkin.DataTable) error {
 		return err
 	}
 
+	time.Sleep(2 * time.Second)
+
 	command = ExecCommand{
 		Command:   example_service_dir + "/scripts/test-call",
 		Directory: example_service_dir,
 	}
 
 	return run_command(command)
+}
+
+func singularitynet_job_is_created(table *gherkin.DataTable) error {
+
+	agent_contract_address := get_table_value(table, "agent contract address")
+	max_price := get_table_value(table, "max price")
+
+	args := []string{
+		"agent",
+		"--at", agent_contract_address,
+		"create-jobs",
+		"--funded",
+		"--signed",
+		"--max-price", max_price,
+	}
+
+	command := ExecCommand{
+		Command:   "snet",
+		Directory: example_service_dir,
+		Input:     []string{"y", "y", "y"},
+		Args:      args,
+	}
+
+	err := run_command(command)
+
+	if err != nil {
+		return err
+	}
+
+	args = []string{
+		"client", "call", "classify",
+		fmt.Sprintf(`{"image_type": "jpg", "image": "%s"}`, test_image),
+		"--agent-at", agent_contract_address,
+	}
+
+	command = ExecCommand{
+		Command:   "snet",
+		Directory: example_service_dir,
+		Args:      args,
+	}
+
+	return run_command(command)
+
+	return nil
 }
 
 func FeatureContext(s *godog.Suite) {
@@ -360,6 +407,7 @@ func FeatureContext(s *godog.Suite) {
 	s.Step(`^example-service is registered$`, example_service_is_registered)
 	s.Step(`^example-service is published to network$`, example_service_is_published_to_network)
 	s.Step(`^example-service is run with snet-daemon$`, exampleservice_is_run_with_snet_daemon)
+	s.Step(`^SingularityNET job is created$`, singularitynet_job_is_created)
 
 }
 
@@ -402,3 +450,31 @@ func get_table_value(table *gherkin.DataTable, column string) string {
 	log.Printf("column: %s has not been found in table", column)
 	return ""
 }
+
+var test_image = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAPDw0PDQ0PDg0PDQ0PDQ0PDQ8ODQ0NFRIWFhUSExUYHyghGB4lJxMWITEhJSor" +
+	"Li4uGB8zODMsNygtLisBCgoKDQ0NDxAPFSsdFRktLSs4LCsrKysrKysrKzcrLSsrKy0tKzcrKysrKysrKystNy0rKysrKysrKysr" +
+	"KysrK//AABEIAOEA4QMBIgACEQEDEQH/xAAcAAEAAQUBAQAAAAAAAAAAAAAAAQIDBAUGBwj/xABAEAEAAgECAgcEBAoLAQAAAAAA" +
+	"AQIDBBEFIQYHEhMxQWFRcZGxMkKBghQiI3KDkqGissEIF1NUYpOjwtHh8BX/xAAWAQEBAQAAAAAAAAAAAAAAAAAAAQL/xAAWEQEB" +
+	"AQAAAAAAAAAAAAAAAAAAEQH/2gAMAwEAAhEDEQA/APcQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+	"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+	"AEJavjHGKabsRe1K3vv2O3aKVnbbfn9scgbRG7jtV0g1XjSKxWfCa0i1fjzYF+OaqfHPMfmxWPkD0Dc3ee//AEs0/SzXn70q6ay0" +
+	"+N7frSlHf7m7iseaZ+tPxlk4r+s/GVHWG7n8NLz4Tf8AWmGTSuSNvyto+9MiVuBqq58sfW329tYU243XHMRmmkbzFYmLecztHKRW" +
+	"3EQkAAAAAAAAAAAAAABby56UibXvWlY8bWmKxH2y13SPjWPRae+fJtO3KlZtFe3f2b+UcpmZ8oiXzv0g41rOOavutPGfUT2prWkX" +
+	"tGGN5+rjjlWvrbefOdvAH0Nk6T6Cv0tdp4/TUl551q8c0mpppq6fU481/wAtE1pbteVZ5uMp1McVmsTP4LW08+xOptvHpvFWB0g6" +
+	"DcQ4Vp41GpjT1xfhGPfusk5LTNpiu2+0bRyhUYmk4jk0/b7jJOObREb18vWPZ4s7F0y1lYiLXrl287d5Fp98xZps/jLGmVSOjv03" +
+	"1EzMzHYjs8opett7b+M9uszt6Ndbp7xKsztfBNd+W+Cm+3q02SWLkNV0tesjicf3f/Jj/lcjrO4p7dNH6H/tyMoQdl/Wjxfy1GGv" +
+	"l+LhhTbrH4raPxtZbf8Aw1rVx6uAdFm6Wa/J9PWZ538fykx8mz6LZb5dXpu8vbJPfYtptabfXj2uQxO06vcXb12jr7dRh393bjcH" +
+	"0qECKAAAAAAAAAAAAAA8O/pBcbvXLp9JWZikaeMt9vC1r3tG3+nHxbrqI4TSmnz55rE5N6Ui3nG9Ztaft3j4Of8A6RXDLxl0urrE" +
+	"93fDGG0+UXpe1o+MX/Y3HUXxuk0y6e1oi2SKZMUT52rE1vX38o+Erg9ecV1xaLvuC62IjnSMeSPSa3iXaxLXdI9H3+j1WKY37eDJ" +
+	"G3rtvHyQfLVrdqtbR9albfGIY9pXcNdsVYnxpNqT762mv8li0tIoySxrr1pWLoKJQlAEKoUK4BfxPSeqLS95r8M+VO1f3bVnb+Tz" +
+	"XD4va+ozQ721OaY5Ux0pE+tpmZ/hQevwAKAAAAAAAAAAAAAA03Szo7i4lpMulz8otzx5Ij8bFlj6N4/94TL5u4jwniHANVtel+xF" +
+	"+1iy07Vcd4ieVsd48J9PH5vqpj63RYs9Jx58VMuO0bWpkrF6z9kg8e4P14460rXV6bJa8RtN68pn37bxLbf138PmOeDUe6YiGx4t" +
+	"1P8AC802tipl0lp/sctrUj3UvvEfY4/inUXkjedJrcWSee1NRinHM/epv8io4HUWre2ovjiYx31WovjiY2mMd8k2pvHumGtyOy4l" +
+	"0O1ugwXnXY6xvetaXpkjJS0RTaNp8fKPGHH6iNpUY9lmy5ZasCiUJlAJhMKd1UAyNNG9ofS/VTwzuOG4rTG1s8zln29nwr8v2vn7" +
+	"olwy2q1enwUjnkyRX3R5z9kRMvqzSYK4sePHSNqY6UpWPZWsbQir4jc3BIjdIAAAAAAAAAAAAAAAANV0m4VGs0ubBPjakzjn2ZI5" +
+	"1l8x8b0VsOTJS9drVtatonymJ2fWEw8f65OjG141uGm9cnLP2a8q5IjlafZv84XDXit1qWTnpsxpEUShMqQSrqt7tl0f4Xk1eoxY" +
+	"MUb3yWiPbFY352n0jxQeudRnAezGXX5K7bTOHTzPtmI7do+O3xevd40PCNPTS4MOnxRtjxUiseU2nztPrM82dXNuK2PeJ7xg1uuR" +
+	"YGZFlcSxK2X6SC8IhIAAAAAAAAAAAAAACm9ItExaImJ8YmN4mFQDlOK9XfCtVM2y6GlbT9fDfJgn9yYhodR1M8Jn6P4VT0jUzaP3" +
+	"ol6SpsDyfN1K6D6up1cfexz/ALWPHUvoonnqdVMe/HH8nrlqLU4geZYOqDhlfpVz5PztRav8OzpODdEtJo940unri35WtG83mPW0" +
+	"zM/tdT3SYxA11NIvV07N7tVFAY1MK53a92U9kFqtF2tVUQkBKEgAAAAAAAAAAAAAAAAI2SApmDZUAp2TskBGxskBAkBAkAAAAAAA" +
+	"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB//2Q=="
