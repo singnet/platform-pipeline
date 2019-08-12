2c65377f205f5620ac0872bcc62648657b17439f
anandrgitnirman <NIrman$123>
2019-08-12 21:12:23 +0530

    https://github.com/singnet/platform-pipeline/issues/38 Updated the test cases to support the new platform contract

--- features/publish_service.feature
+++ features/publish_service.feature
@@ -8,26 +8,26 @@ Feature: Publish example service
             | Ethereum RPC port | user name   | IPFS port |
             | 8545              | "snet-user" | 5002      |
         Given Organization is added:
-            | organization        |
-            | ExampleOrganization |
+            | organization        |etcd endpoint|group name|payment address|
+            | ExampleOrganization |http://127.0.0.1:2379|default_group|0x3b2b3C2e2E7C93db335E69D827F3CC4bC2A2A2cB|
 
     Scenario: Publish example-service-services
         When  example-service service is registered
-            | name            | display name      | organization name   | daemon port |
-            | ExampleService  | Example Service | ExampleOrganization | 8090        |
+            | name            | display name      | organization name   | daemon port |group name|
+            | ExampleService  | Example Service | ExampleOrganization | 8090        |default_group|
         When  example-service service snet-daemon config file is created
-            | name            | organization name   | daemon port | price |
-            | ExampleService  | ExampleOrganization | 8090        | 10    |
+            | name            | organization name   | daemon port | price |daemon group|
+            | ExampleService  | ExampleOrganization | 8090        | 10    |default_group|
         When example-service service is running
         Then example-service make a call using payment channel
-            | name            | organization name   | daemon port |
-            | ExampleService  | ExampleOrganization | 8090        |
+            | group name     | organization name   | daemon port |service name|
+            | default_group  | ExampleOrganization | 8090        |ExampleService|
         Then example-service claim channel by treasurer server
             | daemon port |
             | 8090        |
         Then example-service make a call using payment channel
-            | name            | organization name   | daemon port |
-            | ExampleService  | ExampleOrganization | 8090        |
+            | name            | organization name   | daemon port |service name|
+            | ExampleService  | ExampleOrganization | 8090        |ExampleService|
         Then example-service claim channel by treasurer server
             | daemon port |
             | 8090        |
--- setup_environment_test.go
+++ setup_environment_test.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-
-	"log"
-
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/DATA-DOG/godog/gherkin"
+	"github.com/ethereum/go-ethereum/common"
+	"log"
 )
 
 var platformContractsDir string
@@ -23,17 +21,17 @@ var organizationAddress string
 func init() {
 	platformContractsDir = envSingnetRepos + "/platform-contracts"
 	exampleServiceDir = envSingnetRepos + "/example-service"
-    treasurerServerDir = envSingnetRepos + "/treasurer"
+	treasurerServerDir = envSingnetRepos + "/treasurer"
 	snetConfigFile = envHome + "/.snet/config"
 }
 
 func ethereumNetworkIsRunningOnPort(port int) (err error) {
 
 	output := logPath + "/ganache.log"
-
+	NewCommand().Run("killall node || echo \"supress an error\"")
 	err = NewCommand().Dir(platformContractsDir).
 		Output(output).
-		RunAsync("./node_modules/.bin/ganache-cli --mnemonic \"gauge enact biology destroy normal tunnel slight slide wide sauce ladder produce\"").
+		RunAsync("./node_modules/.bin/ganache-cli --deterministic --mnemonic \"gauge enact biology destroy normal tunnel slight slide wide sauce ladder produce\"").
 		CheckOutput("Listening on 127.0.0.1:" + toString(port)).
 		Err()
 
@@ -113,6 +111,8 @@ func ipfsIsRunning(portAPI int, portGateway int) (err error) {
 	outputFile := logPath + "/ipfs.log"
 
 	err = NewCommand().
+		Run("ipfs shutdown || echo \"supress an error\"").
+		Run("rm -rf ~/.ipfs").
 		Run("ipfs init").
 		Run("ipfs bootstrap rm --all").
 		Run("ipfs config Addresses.API %s", addressAPI).
@@ -120,40 +120,48 @@ func ipfsIsRunning(portAPI int, portGateway int) (err error) {
 		Output(outputFile).
 		RunAsync("ipfs daemon").
 		CheckOutput(
-		"Daemon is ready",
-		"server listening on " + addressAPI,
-		"server listening on " + addressGateway).
+			"Daemon is ready",
+			"server listening on "+addressAPI,
+			"server listening on "+addressGateway).
 		Err()
-
 
 	return
 }
 
 func snetIsConfiguredLocalRpc(table *gherkin.DataTable) (err error) {
-    rpc_port  := getTableValue(table, "Ethereum RPC port")
-    user_name := getTableValue(table, "user name")
-    ipfs_port := getTableValue(table, "IPFS port")
-    err = NewCommand().
-          Run("snet network create local http://localhost:%s", rpc_port).
-          Run("snet identity create %s rpc --network local", user_name).
-          Run("snet set default_ipfs_endpoint http://localhost:%s", ipfs_port).
-          Run("snet set current_singularitynettoken_at " + singnetTokenAddress).
-          Run("snet set current_registry_at "            + registryAddress).
-          Run("snet set current_multipartyescrow_at "    + multiPartyEscrow).
-          Err()
+	rpc_port := getTableValue(table, "Ethereum RPC port")
+	user_name := getTableValue(table, "user name")
+	ipfs_port := getTableValue(table, "IPFS port")
+
+	err = NewCommand().
+		Run("rm -rf ~/.snet").
+		Run("snet network create local http://localhost:%s", rpc_port).
+		Run("snet identity create %s rpc --network local", user_name).
+		Run("snet set default_ipfs_endpoint http://localhost:%s", ipfs_port).
+		Run("snet set current_singularitynettoken_at " + singnetTokenAddress).
+		Run("snet set current_registry_at " + registryAddress).
+		Run("snet set current_multipartyescrow_at " + multiPartyEscrow).
+		Err()
 	return
 }
-
 
 func organizationIsAdded(table *gherkin.DataTable) (err error) {
 
 	organization := getTableValue(table, "organization")
+	etcd_endpoint := getTableValue(table, "etcd endpoint")
+	group_name := getTableValue(table, "group name")
+	payment_address := getTableValue(table, "payment address")
+	//snet organization add-group group1 0x42A605c07EdE0E1f648aB054775D6D4E38496144 5.5.6.7:8089
 
-	return NewCommand().
-		Run("snet organization create %s -y --org-id %s", organization, organization).
+	err = NewCommand().
+		Run("snet organization metadata-init %s %s", organization, organization).
+		Run("snet organization add-group %s %s %s ", group_name, payment_address, etcd_endpoint).
+		Run("snet organization create %s -y", organization).
 		Err()
-}
 
+	return
+
+}
 
 func getTableValue(table *gherkin.DataTable, column string) string {
 
